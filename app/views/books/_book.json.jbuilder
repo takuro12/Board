66fa004712e21a1d2679288ea1821c35c47da111
EC2 Default User <ec2-user@ip-172-31-10-224.us-east-2.compute.internal>
2018-12-30 08:19:14 +0000

    first commit

---
+++ app/views/books/_book.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! book, :id, :country, :induction, :created_at, :updated_at
+json.url book_url(book, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_27_080635) do
+ActiveRecord::Schema.define(version: 2018_12_29_075011) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "country"
+    t.string "induction"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
